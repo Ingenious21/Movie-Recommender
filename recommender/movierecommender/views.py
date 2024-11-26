8621514ffecddbba9d75ec6f38a9d34c127f700f
Nehemiah Kemayah <ingeniouskemayah@gmail.com>
2024-11-26 21:30:26 +0000

    working

--- recommender/movierecommender/views.py
+++ recommender/movierecommender/views.py
@@ -3,4 +3,10 @@ from .models import Movie
 from django.shortcuts import render
 
 # HINT: Create a view to provide movie recommendations list for the HTML template
+def movie_recommendation_view(request):
+    if request.method == "GET":
+        context = {}
+        #The context/data to be presented is in the HTML template
+        # #Render an HTML page with specified template and context
+        return render(request, 'movierecommender/movielist.html', context)
 
